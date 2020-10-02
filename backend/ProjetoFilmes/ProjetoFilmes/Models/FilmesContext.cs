25e4837d32defa8b81e159f194d5e67d4b7e8277
Rodhenrique <henriquesilvasenai@gmail.com>
2020-10-02 15:36:29 -0300

    beta

---
+++ backend/ProjetoFilmes/ProjetoFilmes/Models/FilmesContext.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProjetoFilmes.Models
+{
+    public partial class FilmesContext : DbContext
+    {
+        public FilmesContext()
+        {
+        }
+
+        public FilmesContext(DbContextOptions<FilmesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Filmes> Filmes { get; set; }
+        public virtual DbSet<Generos> Generos { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=LAPTOP-OEOULMOC\\SQLEXPRESS;Database=Filmes;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Filmes>(entity =>
+            {
+                entity.HasKey(e => e.IdFilme);
+
+                entity.HasIndex(e => e.Titulo)
+                    .HasName("UQ__Filmes__7B406B56FDD2F7CA")
+                    .IsUnique();
+
+                entity.Property(e => e.Titulo)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdGeneroNavigation)
+                    .WithMany(p => p.Filmes)
+                    .HasForeignKey(d => d.IdGenero)
+                    .HasConstraintName("FK__Filmes__IdGenero__286302EC");
+            });
+
+            modelBuilder.Entity<Generos>(entity =>
+            {
+                entity.HasKey(e => e.IdGenero);
+
+                entity.HasIndex(e => e.Nome)
+                    .HasName("UQ__Generos__7D8FE3B210FA8DC0")
+                    .IsUnique();
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("UQ__Usuarios__A9D10534F3768DC4")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Permissao)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
